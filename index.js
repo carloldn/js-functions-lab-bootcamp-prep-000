95de52ae675f6aaf59706b263a07f29f329fa92d
carlo montanaro <carloldn@outlook.com>
2018-09-11 16:36:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 // write your code below!
 function happyHolidays () {
 return "Happy holidays!"}
+function happyHolidaysTo (Susan) {
+  return "Happy holidays, Susan!"}
+function happyHolidayTo (holiday, Susan) {
+  return "Happy Christmas,Susan!"
+}
+function HolidayCountdown (Christmas, five) {
+  return "It's 5 days until Christmas"
+}
