5d5dc6a2f577850bac79564cfe6d7e513985e6e6
carlo montanaro <carloldn@outlook.com>
2018-09-11 16:32:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 // write your code below!
 function happyHolidays () {
 return "Happy holidays!"}
-function happyHolidaysTo (name) {
-  return "Happy holidays, Susan!"}
-function happyHolidayTo (holiday) {
-  return "Happy Christmas,Susan!"
-}
